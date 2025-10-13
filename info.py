5bdd5ff42125c6aaced3f31072204df520934a94
Tejas Jadhav <tejasjadhav@Admins-MacBook-Pro.local>
2025-10-13 16:48:13 -0400

    updated info.py to have names that can be pickedup randomly so that llm does not have to handle that part

--- info.py
+++ info.py
@@ -2,24 +2,23 @@
 COMPANIES = {
     1: {
         "greeting": "Thank you for calling Mister Sparky! My name is {name}, how can I make you smile today?",
-        "careers_url": "mistersparky.com/careers",
+        "careers_url": "https://mistersparky.com/careers",
         "careers_email": "careers@mistersparky.com",
-        "voice_id":"egTToTzW6GojvddLj0zd",
-        "gender":"M"
-
+        "voice_id": "egTToTzW6GojvddLj0zd",
+        "names": ["John", "Michael", "David", "James", "Tejas"]
     },
     2: {
         "greeting": "Thank you for calling Del-Air Heating and Air. My name is {name}, how can I make you smile today?",
-        "careers_url": "delair.com/careers",
+        "careers_url": "https://delair.com/careers",
         "careers_email": "careers@delair.com",
-        "voice_id":"egTToTzW6GojvddLj0zd",
-        "gender":"M"
+        "voice_id": "egTToTzW6GojvddLj0zd",
+        "names": ["Robert", "Daniel", "Mark", "Alex", "Kevin"]
     },
     3: {
         "greeting": "Thank you for calling Benjamin Franklin Plumbing! My name is {name}, how can I make you smile today?",
-        "careers_url": "benjaminfranklinplumbing.com/careers",
+        "careers_url": "https://benjaminfranklinplumbing.com/careers",
         "careers_email": "careers@benfranklinplumbing.com",
-        "voice_id":"egTToTzW6GojvddLj0zd",
-        "gender":"M"
+        "voice_id": "egTToTzW6GojvddLj0zd",
+        "names": ["William", "James", "Ethan", "Jacob", "Michael"]
     }
 }
